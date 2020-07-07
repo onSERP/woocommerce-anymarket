d96ae5d73caff99c34ad391a6232f024f21d7c27
Gustavo <gustavo_641@live.com>
2020-07-07 08:47:52 -0300

    create rest routes

---
+++ app/routes/rest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WordPress REST API Routes.
+ *
+ * @package Anymarket
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+//register notification routes
+register_rest_route( 'anymarket/v1', '/notifications', [
+    'methods' => WP_REST_Server::CREATABLE,
+	'callback' =>['\\Anymarket\\Controllers\\Rest\\NotificationController', 'create'],
+] );
+
+//register options routes
+register_rest_route( 'anymarket/v1', '/options', [
+    'methods' => WP_REST_Server::READABLE,
+	'callback' =>['\\Anymarket\\Controllers\\Rest\\OptionController', 'index'],
+	'permission_callback' => function () {
+		return current_user_can( 'manage_options' );
+		}
+] );
+
+register_rest_route( 'anymarket/v1', '/options', [
+    'methods' => WP_REST_Server::EDITABLE,
+	'callback' =>['\\Anymarket\\Controllers\\Rest\\OptionController', 'edit'],
+	'permission_callback' => function () {
+		return current_user_can( 'manage_options' );
+		}
+] );
---
+++ app/src/Controllers/Rest/NotificationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Anymarket\Controllers\Rest;
+
+/**
+ *
+ */
+class NotificationController
+{
+
+	public static function create( \WP_REST_Request $request ){
+
+	}
+}
