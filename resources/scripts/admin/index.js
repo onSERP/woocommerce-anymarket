a056773f8430b3de73cbcbf002d6f5f448ff1556
Gustavo <gustavo_641@live.com>
2020-07-25 01:04:27 -0300

    button see on anymarket - products
    also refactoring

--- resources/scripts/admin/index.js
+++ resources/scripts/admin/index.js
@@ -2,32 +2,55 @@
 import config from '@config'
 import '@styles/admin'
 
-// button on product categories
+/**
+ * Create button element
+ *
+ * @param {string} page
+ * @returns {object} a
+ */
+function createButton(page) {
+  const protocol = anymarket_is_sandbox ? 'http://' : 'https://'
+  const subdomain = anymarket_is_sandbox ? 'sandbox' : 'api'
+  const link = `${protocol}${subdomain}.anymarket.com.br/#/${page}`
 
-function buttonOnCategories() {
+  const a = document.createElement('a')
+  a.setAttribute('href', link)
+  a.setAttribute('target', '_blank')
+  a.classList.add('page-title-action')
+  a.innerText = 'Ver no Anymarket '
+  a.style.display = 'inline-block'
+  a.style.paddingBottom = '1px'
+
+  const span = document.createElement('span')
+  span.classList.add('dashicons', 'dashicons-external')
+
+  a.append(span)
+
+  return a
+}
+
+/**
+ * Append button on page
+ *
+ * @param {string} path path
+ * @param {string} param query param
+ * @param {string} value query value
+ * @param {string} dest destination
+ *
+ * @returns {void}
+ */
+function buttonOnPage(path, param, value, dest) {
   const params = new URL(document.location).searchParams
+  const pathname = '/wp-admin/' + path
 
-  if (params.get('taxonomy') === 'product_cat') {
+  if (window.location.pathname === pathname && params.get(param) === value) {
     const headingInline = document.querySelector('.wp-heading-inline')
-
-    const protocol = anymarket_is_sandbox ? 'http://' : 'https://'
-    const subdomain = anymarket_is_sandbox ? 'sandbox' : 'api'
-    const link = `${protocol}${subdomain}.anymarket.com.br/#/categories`
-
-    const a = document.createElement('a')
-    a.setAttribute('href', link)
-    a.setAttribute('target', '_blank')
-    a.classList.add('page-title-action')
-    a.innerText = 'Ver no Anymarket '
-    a.style.display = 'inline-block'
-
-    const span = document.createElement('span')
-    span.classList.add('dashicons', 'dashicons-external')
-
-    a.append(span)
-
-    headingInline.after(a)
+    headingInline.after(createButton(dest))
   }
 }
 
-buttonOnCategories()
+// button on product categories
+buttonOnPage('edit-tags.php', 'taxonomy', 'product_cat', 'categories')
+
+// button on products list
+buttonOnPage('edit.php', 'post_type', 'product', 'products/list')
