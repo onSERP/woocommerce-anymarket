64b88ccf41718969de8cd9fc13a0f9852d5f2ecf
Gustavo <gustavo@agenciaazul.com.br>
2020-08-10 14:41:16 -0300

    export stock on save product/product variation

--- app/src/Anymarket/ExportStock.php
+++ app/src/Anymarket/ExportStock.php
@@ -5,7 +5,7 @@ namespace Anymarket\Anymarket;
 /**
  * Export products
  */
-class ExportStock extends ExportService implements ExportInterface
+class ExportStock extends ExportService
 {
 	/**
 	 * Undocumented function
@@ -78,9 +78,15 @@ class ExportStock extends ExportService implements ExportInterface
 
 	}
 
+	/**
+	 * Undocumented function
+	 *
+	 * @param array $products
+	 * @return void
+	 */
 	public function exportProductStock( array $products ){
 
-		$product = $product[0];
+		$product = $products[0];
 
 		$data = [];
 
@@ -92,67 +98,64 @@ class ExportStock extends ExportService implements ExportInterface
 			]);
 
 			foreach ($variations as $variation){
-				$product_variation = new \WC_Product_Variation( $variation );
+				$product_variation = new \WC_Product_Variation( $variation->ID );
+				$product_price = $product_variation->get_price();
+				$product_quantity = $product_variation->get_stock_quantity();
+				$id = get_post_meta($variation->ID, 'anymarket_variation_id', true);
 
-
+				if( !empty($id) ){
+					$data[] = [
+						'id' => intval($id),
+						'quantity' => $product_quantity,
+						'cost' => $product_price,
+					];
+				}
 
 			}
 
-
-
-
 		} else {
+			$product_price = $product->get_price();
+			$id = carbon_get_post_meta( $product->get_ID(), 'anymarket_variation_id' );
+			$product_quantity = $product->get_stock_quantity();
 
+			if( !empty($id) ){
+				$data[] = [
+					'id' => intval($id),
+					'quantity' => $product_quantity,
+					'cost' => $product_price,
+				];
+			}
 		}
 
+		if( empty( $data) ) return;
 
+		$this->curl->put($this->baseUrl . 'stocks', json_encode($data, JSON_UNESCAPED_UNICODE));
 
-		$product_price = $product_variation === 0 ? $product->get_price() : $product_variation->get_price();
+		$report = [];
 
-		$id = $product_variation !== 0 ? get_post_meta($stock_item['variation_id'], 'anymarket_variation_id', true) : carbon_get_post_meta( $stock_item['item_id'], 'anymarket_variation_id' );
-
-		$quantity = $product_variation !== 0 ? $product_variation->get_stock_quantity() : $product->get_stock_quantity();
-
-		// returns [id, amount] or false
-		//$stockLocalId = $this->getStockLocalId( $id, $quantity );
-
-		if( !empty($id) ){
-			$data[] = [
-				'id' => intval($id),
-				'quantity' => $quantity,
-				'cost' => $product_price,
+		if($this->curl->error){
+			$report[] = [
+				'order' => $order_ids[0],
+				'type' => 'Update stock',
+				'url' => $this->curl->url,
+				'errorCode' => $this->curl->errorCode,
+				'errorMessage' => $this->curl->response->message,
+				'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
+			];
+		} else {
+			$report[] = [
+				'order' => $order_ids[0],
+				'type' => 'Update stock',
+				'url' => $this->curl->url,
+				'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
+				'response' => $this->curl->response,
+				'responseCode' => $this->curl->httpStatusCode
 			];
 		}
 
-	if( empty( $data) ) return;
-
-	$this->curl->put($this->baseUrl . 'stocks', json_encode($data, JSON_UNESCAPED_UNICODE));
-
-	$report = [];
-
-	if($this->curl->error){
-		$report[] = [
-			'order' => $order_ids[0],
-			'type' => 'Update stock',
-			'url' => $this->curl->url,
-			'errorCode' => $this->curl->errorCode,
-			'errorMessage' => $this->curl->response->message,
-			'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
-		];
-	} else {
-		$report[] = [
-			'order' => $order_ids[0],
-			'type' => 'Update stock',
-			'url' => $this->curl->url,
-			'data' => json_encode($data, JSON_UNESCAPED_UNICODE),
-			'response' => $this->curl->response,
-			'responseCode' => $this->curl->httpStatusCode
-		];
-	}
-
-	if( get_option('anymarket_is_dev_env') == 'true' ){
-		$this->logger->debug( print_r($report, true), ['source' => 'woocommerce-anymarket']);
-	}
+		if( get_option('anymarket_is_dev_env') == 'true' ){
+			$this->logger->debug( print_r($report, true), ['source' => 'woocommerce-anymarket']);
+		}
 
 	}
 }
