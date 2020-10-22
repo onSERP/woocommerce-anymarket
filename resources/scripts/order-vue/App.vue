0b8a36a0f2ac28feac8af71ea92ff6d9f140fd0f
Gustavo Rocha <gustavo@onserp.com.br>
2020-10-22 16:42:29 -0300

    cleanup

--- resources/scripts/order-vue/App.vue
+++ resources/scripts/order-vue/App.vue
@@ -1,23 +1,11 @@
 <template>
-  <div id="carbon_fields_container_order_data_vue">
-    <OrderDetails />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import OrderDetails from './components/OrderDetails'
 export default {
   name: 'App',
-  components: {
-    OrderDetails,
-  },
 }
 </script>
 
-<style lang="scss">
-#carbon_fields_container_order_data_vue {
-  .toggle-indicator {
-    display: none;
-  }
-}
-</style>
+<style lang="scss"></style>
--- resources/scripts/order-vue/index.js
+++ resources/scripts/order-vue/index.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueGoodTablePlugin from 'vue-good-table'
-
+import domReady from '@wordpress/dom-ready'
 // import the styles
-import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.config.productionTip = false
 
 //vue
-wp.domReady(function () {
-  Vue.use(VueGoodTablePlugin)
-
+domReady(function () {
   new Vue({
-    el: '#carbon_fields_container_order_data_vue .inside',
+    el: '#notAnElement',
     render: (h) => h(App),
   })
 })
