6a9f569af4d193484499de9cdf5e0c2f3dc75817
Gustavo <gustavo_641@live.com>
2021-06-24 16:26:04 -0300

    improve logging cron events

--- app/src/WordPress/CronEvents.php
+++ app/src/WordPress/CronEvents.php
@@ -12,7 +12,7 @@ class CronEvents
 {
 
 	public function init(){
-		add_action( 'anymarket_cron_export_products_on_save', [$this, 'exportProd'] );
+		add_action( 'anymarket_cron_export_products_on_save', [$this, 'exportProd'], 10, 1 );
 		add_action( 'anymarket_cron_bulk_export_products', [$this, 'bulkExportProd'], 10, 3 );
 
 		add_action( 'anymarket_cron_export_orders_on_save', [$this, 'exportOrder'], 10 , 2 );
@@ -25,7 +25,11 @@ class CronEvents
    * @return void
    */
  	public static function setCronExportProd( int $time, array $args ){
+		$log = wc_get_logger();
+
   	  	wp_schedule_single_event( time() + $time, 'anymarket_cron_export_products_on_save', $args );
+
+		$log->debug( print_r(['cron' => '`setCronExportProd` agendado', 'argumentos' => $args], true), ['source' => 'woocommerce-anymarket-cron']);
   	}
 
   /**
@@ -35,7 +39,13 @@ class CronEvents
    */
 	public function exportProd( $post_id ){
 		$exportProducts = new ExportProducts();
-		$response = is_array($post_id[0]) ? $exportProducts->export( $post_id ) : $exportProducts->export( [$post_id] );
+		$log = wc_get_logger();
+		$log->debug( print_r([
+			'cron' => '`exportProd` executando',
+			'argumentos' => ['post id(s)' => $post_id]],
+		true), ['source' => 'woocommerce-anymarket-cron']);
+
+		$response = is_array($post_id) ? $exportProducts->export( $post_id ) : $exportProducts->export( [$post_id] );
 
 		if( is_wp_error($response) ){
 			set_transient( 'anymarket_product_export_fail', $response->get_error_message(), MINUTE_IN_SECONDS );
@@ -51,7 +61,11 @@ class CronEvents
    * @return void
    */
 	public static function setCronBulkExportProd( int $time, array $args ){
+		$log = wc_get_logger();
+
 		wp_schedule_single_event( time() + $time, 'anymarket_cron_bulk_export_products', $args );
+
+		$log->debug( print_r(['cron' => '`setCronBulkExportProd` agendado', 'argumentos' => $args], true), ['source' => 'woocommerce-anymarket-cron']);
 	}
 
   /**
@@ -61,7 +75,13 @@ class CronEvents
    * @return void
    */
   	public function bulkExportProd( $object_ids, $update = false, $update_args = [] ){
-		$exportProducts = new ExportProducts;
+		$exportProducts = new ExportProducts();
+		$log = wc_get_logger();
+		$log->debug( print_r([
+			'cron' => '`bulkExportProd` executando',
+			'argumentos' => ['ids' => $object_ids, 'update' => $update, 'update args' => $update_args]],
+			true), ['source' => 'woocommerce-anymarket-cron']);
+
 
 		$response = $exportProducts->export( $object_ids, $update, $update_args );
 
@@ -73,11 +93,21 @@ class CronEvents
   	}
 
 	public static function setCronExportOrder( int $time, array $args ){
+		$log = wc_get_logger();
+
 		wp_schedule_single_event( time() + $time, 'anymarket_cron_export_orders_on_save', $args );
+
+		$log->debug( print_r(['cron' => '`setCronExportOrder` agendado', 'argumentos' => $args], true), ['source' => 'woocommerce-anymarket-cron']);
 	}
 
 	public function exportOrder( $order_id, $new_status ){
-		$order = new AnymarketOrder;
+		$order = new AnymarketOrder();
+		$log = wc_get_logger();
+		$log->debug( print_r([
+			'cron' => '`exportOrder` executando',
+			'argumentos' => ['order id' => $order_id, 'new status' => $new_status]],
+			true), ['source' => 'woocommerce-anymarket-cron']);
+
 		$order->updateStatus( $order_id, $new_status );
 	}
 }
